a3fc87e03294c011184e41ab37aae5b7d8e2ca02
Ian May <imay@lanl.gov>
2025-02-27 16:52:25 -0700

    Add cell-based dof managers to remote dofs test

--- src/discretization/test/test_RemoteDOFs.cpp
+++ src/discretization/test/test_RemoteDOFs.cpp
@@ -12,52 +12,35 @@
 
 #include <set>
 
-void remoteDOFTest( AMP::UnitTest *ut, std::string input_file )
+void testPair( AMP::UnitTest *ut,
+               std::shared_ptr<AMP::Discretization::DOFManager> leftDOF,
+               std::shared_ptr<AMP::Discretization::DOFManager> rightDOF )
 {
-    AMP::pout << "Testing vertex remote DOFs with input: " << input_file << std::endl;
+    auto comm = leftDOF->getComm();
 
-    // Read the input file
-    auto input_db = AMP::Database::parseInputFile( input_file );
-
-    // Get the Mesh database and create the mesh parameters
-    auto database = input_db->getDatabase( "Mesh" );
-    auto params   = std::make_shared<AMP::Mesh::MeshParameters>( database );
-    auto comm     = AMP::AMP_MPI( AMP_COMM_WORLD );
-    params->setComm( comm );
-    if ( comm.getSize() == 1 ) {
-        ut->passes( "Trivial pass for single rank runs" );
-        return;
-    }
-
-    // Create the meshes from the input database
-    auto mesh = AMP::Mesh::MeshFactory::create( params );
-
-    // Create the DOF manager
-    auto scalarDOFs =
-        AMP::Discretization::simpleDOFManager::create( mesh, AMP::Mesh::GeomType::Vertex, 1, 1 );
-
-    // For the remote dofs inside scalarDOFs to be valid two things must be true
+    // For the remote dofs inside rightDOF to be valid two things must be true
     // 1. Every referenced remote dof must be present
     // 2. Every present remote dof must be referenced
     // By corollary the two sets will be the same size
 
-    const size_t start = scalarDOFs->beginDOF(), end = scalarDOFs->endDOF();
+    const size_t start_row = leftDOF->beginDOF(), end_row = leftDOF->endDOF();
+    const size_t start_col = rightDOF->beginDOF(), end_col = rightDOF->endDOF();
 
-    // stick all remote dofs in scalarDOFs into a set
+    // stick all remote dofs in rightDOF into a set
     std::set<size_t> stored_rDOFs;
-    for ( auto &&rdof : scalarDOFs->getRemoteDOFs() ) {
+    for ( auto &&rdof : rightDOF->getRemoteDOFs() ) {
         stored_rDOFs.insert( rdof );
     }
     const size_t num_stored = stored_rDOFs.size();
 
     // get all remote dofs that are actually referenced
     bool pass_ref_in_stored = true;
-    auto is_remote          = [start, end]( const size_t rdof ) -> bool {
-        return rdof < start || rdof >= end;
+    auto is_remote          = [start_col, end_col]( const size_t rdof ) -> bool {
+        return rdof < start_col || rdof >= end_col;
     };
     std::set<size_t> refd_rDOFs;
-    for ( size_t row = start; row < end; ++row ) {
-        auto row_dofs = scalarDOFs->getRowDOFs( scalarDOFs->getElementID( row ) );
+    for ( size_t row = start_row; row < end_row; ++row ) {
+        auto row_dofs = rightDOF->getRowDOFs( leftDOF->getElementID( row ) );
         for ( auto &&rdof : row_dofs ) {
             if ( is_remote( rdof ) ) {
                 refd_rDOFs.insert( rdof );
@@ -93,15 +76,49 @@ void remoteDOFTest( AMP::UnitTest *ut, std::string input_file )
         ut->failure( "DOFManager contains un-referenced remote DOFs" );
     }
 
+    comm.barrier();
+
     if ( !pass_ref_in_stored || !pass_stored_is_refd || true ) {
         std::cout << "Rank " << comm.getRank() << " stored " << num_stored << " DOFs, referenced "
-                  << num_refd << " DOFs, with " << scalarDOFs->numLocalDOF() << " local DOFs and "
-                  << scalarDOFs->numGlobalDOF() << " global DOFs" << std::endl;
+                  << num_refd << " DOFs, with " << rightDOF->numLocalDOF() << " local DOFs and "
+                  << rightDOF->numGlobalDOF() << " global DOFs" << std::endl;
     }
     comm.barrier();
 }
 
-// Main function
+void remoteDOFTest( AMP::UnitTest *ut, std::string input_file )
+{
+    // Read the input file
+    auto input_db = AMP::Database::parseInputFile( input_file );
+
+    // Get the Mesh database and create the mesh parameters
+    auto database = input_db->getDatabase( "Mesh" );
+    auto params   = std::make_shared<AMP::Mesh::MeshParameters>( database );
+    auto comm     = AMP::AMP_MPI( AMP_COMM_WORLD );
+    params->setComm( comm );
+    if ( comm.getSize() == 1 ) {
+        ut->passes( "Trivial pass for single rank runs" );
+        return;
+    }
+
+    comm.barrier();
+    AMP::pout << "Testing vertex remote DOFs with input: " << input_file << std::endl;
+    comm.barrier();
+
+    // Create the meshes from the input database
+    auto mesh = AMP::Mesh::MeshFactory::create( params );
+
+    // Create DOFManagers over different entities
+    auto vertexDOFs =
+        AMP::Discretization::simpleDOFManager::create( mesh, AMP::Mesh::GeomType::Vertex, 1, 1 );
+    auto cellDOFs =
+        AMP::Discretization::simpleDOFManager::create( mesh, AMP::Mesh::GeomType::Cell, 1, 1 );
+
+    // test combinations of managers
+    testPair( ut, vertexDOFs, vertexDOFs );
+    testPair( ut, cellDOFs, cellDOFs );
+}
+
 int main( int argc, char **argv )
 {
     AMP::AMPManager::startup( argc, argv );
