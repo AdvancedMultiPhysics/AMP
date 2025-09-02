703d83ebf52623e2b93ad74652f4fb0f9e2e9997
Bobby Philip <bphilip@lanl.gov>
2025-09-02 09:43:02 -0600

    intermediate save

--- src/time_integrators/test/testSimpleTimeIntegration.cpp
+++ src/time_integrators/test/testSimpleTimeIntegration.cpp
@@ -167,10 +167,8 @@ int testSimpleTimeIntegration( int argc, char *argv[] )
 
     // List of integrators
     // We need to look at the errors for the first order -- whether they are acceptable
-    // auto integrators = { "ExplicitEuler", "Backward Euler" "CN",
-    //    "RK2", "RK23", "RK34", "RK4", "RK45", "BDF1", "BDF2", "BDF3", "BDF4", "BDF5" };
     auto integrators = { "ExplicitEuler", "RK2",  "RK4",  "RK12", "RK23", "RK34", "RK45", "CN",
-                         "BDF1",          "BDF2", "BDF3", "BDF4", "BDF5" };
+                         "BDF1",          "BDF2", "BDF3", "BDF4", "BDF5", "BDF6" };
 
     // Run the tests
     for ( auto tmp : integrators )
