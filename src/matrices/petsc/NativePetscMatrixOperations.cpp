a9a013ff442888c65d220227d2daa6b47ea8fc63
Mark Berrill <berrillma@ornl.gov>
2025-09-18 16:19:31 -0400

    Fixing new bug

--- src/matrices/petsc/NativePetscMatrixOperations.cpp
+++ src/matrices/petsc/NativePetscMatrixOperations.cpp
@@ -143,19 +143,19 @@ void NativePetscMatrixOperations::copy( const MatrixData &X, MatrixData &Y )
     MatCopy( getMat( X ), getMat( Y ), SAME_NONZERO_PATTERN );
 }
 
-void NativePetscMatrixOperations::scale( AMP::Scalar, std::shared_ptr<const Vector>, MatrixData & ) override
+void NativePetscMatrixOperations::scale( AMP::Scalar, std::shared_ptr<const Vector>, MatrixData & )
 {
     AMP_ERROR( "Not implemented" );
 }
-void NativePetscMatrixOperations::scaleInv( AMP::Scalar, std::shared_ptr<const Vector>, MatrixData & ) override
+void NativePetscMatrixOperations::scaleInv( AMP::Scalar, std::shared_ptr<const Vector>, MatrixData & )
 {
     AMP_ERROR( "Not implemented" );
 }
-void NativePetscMatrixOperations::getRowSums( MatrixData const &, std::shared_ptr<Vector> ) override
+void NativePetscMatrixOperations::getRowSums( MatrixData const &, std::shared_ptr<Vector> )
 {
     AMP_ERROR( "Not implemented" );
 }
-void NativePetscMatrixOperations::getRowSumsAbsolute( MatrixData const &, std::shared_ptr<Vector> ) override
+void NativePetscMatrixOperations::getRowSumsAbsolute( MatrixData const &, std::shared_ptr<Vector> )
 {
     AMP_ERROR( "Not implemented" );
 }
