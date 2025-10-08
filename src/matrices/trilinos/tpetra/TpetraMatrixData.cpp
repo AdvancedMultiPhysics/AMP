3e4123a55feff152a3838d4bf3df863ef0264e2f
Bobby Philip <bphilip@lanl.gov>
2025-10-08 12:27:09 -0600

    linking version

--- src/matrices/trilinos/tpetra/TpetraMatrixData.cpp
+++ src/matrices/trilinos/tpetra/TpetraMatrixData.cpp
@@ -2,8 +2,32 @@
 
 namespace AMP::LinearAlgebra {
 
-template class TpetraMatrixData<double, int32_t, int64_t>;
+#if defined( HAVE_TPETRA_INST_DOUBLE )
+using ST = double;
+#elif defined( HAVE_TPETRA_INST_FLOAT )
+using ST = float;
+#elif defined( HAVE_TPETRA_INST_LONG_DOUBLE )
+using ST = long double;
+#else
+    #error "Tpetra not configured for given scalar type"
+#endif
 
-// template class TpetraMatrixData<float, int32_t, int64_t>;
+#if defined( HAVE_TPETRA_INST_INT_INT )
+using LO = int32_t;
+using GO = int32_t;
+#elif defined( HAVE_TPETRA_INST_INT_UNSIGNED )
+using LO = int32_t;
+using GO = uint32_t;
+#elif defined( HAVE_TPETRA_INST_INT_LONG )
+using LO = int32_t;
+using GO = long;
+#elif defined( HAVE_TPETRA_INST_INT_LONG_LONG )
+using LO = int32_t;
+using GO = long long;
+#else
+    #error "Tpetra not configured for given local and global ordinal types"
+#endif
+
+template class TpetraMatrixData<ST, LO, GO>;
 
 } // namespace AMP::LinearAlgebra
