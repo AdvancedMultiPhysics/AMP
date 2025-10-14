897df031aacb96f6b4bc0db39332f63ade52b6ca
Jesus Bonilla de Toro <jbonilla@lanl.gov>
2025-10-14 16:17:46 -0600

    Add amp@4.0.0 in v2 spack package

--- spack-repo/v2/spack_repo/amp/packages/tpl_builder/package.py
+++ spack-repo/v2/spack_repo/amp/packages/tpl_builder/package.py
@@ -20,6 +20,7 @@ class TplBuilder(CMakePackage, CudaPackage, ROCmPackage):
     license("UNKNOWN")
 
     version("master", branch="master")
+    version("2.1.2", tag="2.1.2", commit="cdb270395e1512da2f18a34a7fa6b60f1bcb790d")
     version("2.1.0", tag="2.1.0", commit="f2018b32623ea4a2f61fd0e7f7087ecb9b955eb5")
 
     variant("stacktrace", default=False, description="Build with support for Stacktrace")
@@ -80,10 +81,12 @@ class TplBuilder(CMakePackage, CudaPackage, ROCmPackage):
     for _flag in list(CudaPackage.cuda_arch_values):
         depends_on(f"hypre cuda_arch={_flag}", when=f"+hypre+cuda cuda_arch={_flag}")
         depends_on(f"kokkos cuda_arch={_flag}", when=f"+kokkos+cuda cuda_arch={_flag}")
+        depends_on(f"trilinos cuda_arch={_flag}", when=f"+trilinos+cuda cuda_arch={_flag}")
 
     for _flag in ROCmPackage.amdgpu_targets:
         depends_on(f"hypre amdgpu_target={_flag}", when=f"+hypre+rocm amdgpu_target={_flag}")
         depends_on(f"kokkos amdgpu_target={_flag}", when=f"+kokkos+rocm amdgpu_target={_flag}")
+        depends_on(f"trilinos amdgpu_target={_flag}", when=f"+trilinos+rocm amdgpu_target={_flag}")
 
     # MPI related dependencies
     depends_on("mpi", when="+mpi")
