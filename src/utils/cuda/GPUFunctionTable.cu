74fae06090f73b24888ba8b144ffb8ed6324dbb3
Bobby Philip <bphilip@lanl.gov>
2025-10-29 12:41:43 -0600

    fix stray errors

--- src/utils/cuda/GPUFunctionTable.cu
+++ src/utils/cuda/GPUFunctionTable.cu
@@ -277,8 +277,9 @@ bool equalsW(const TYPE* d_a, const TYPE* d_b, TYPE tol, size_t n)
 
 //Explicit Instantiation of the wrappers
 template double sumW<double>(const double* d_a, size_t n);
-template float sumW<float>(const float* d_a, size_t n);
 template bool equalsW<double>(const double* d_a, const double* d_b, double tol, size_t n);
+
+template float sumW<float>(const float* d_a, size_t n);
 template bool equalsW<float>(const float* d_a, const float* d_b, float tol, size_t n);
 
 template void transformReLUW(const double* d_a, double* d_b, size_t n);
@@ -287,6 +288,13 @@ template void transformTanhW(const double* d_a, double* d_b, size_t n);
 template void transformHardTanhW(const double* d_a, double* d_b, size_t n);
 template void transformSigmoidW(const double* d_a, double* d_b, size_t n);
 template void transformSoftPlusW(const double* d_a, double* d_b, size_t n);
+
+template void transformReLUW(const float* d_a, float* d_b, size_t n);
+template void transformAbsW(const float* d_a, float* d_b, size_t n);
+template void transformTanhW(const float* d_a, float* d_b, size_t n);
+template void transformHardTanhW(const float* d_a, float* d_b, size_t n);
+template void transformSigmoidW(const float* d_a, float* d_b, size_t n);
+template void transformSoftPlusW(const float* d_a, float* d_b, size_t n);
 
 }
 
--- src/utils/hip/GPUFunctionTable.cpp
+++ src/utils/hip/GPUFunctionTable.cpp
@@ -1,66 +1,7 @@
 #include "AMP/utils/hip/GPUFunctionTable.hpp"
 namespace AMP {
 
-template<>
-void GPUFunctionTable::rand<int>( size_t n, int *d_x )
-{
-    hiprandGenerator_t gen;
-    hiprandCreateGenerator( &gen, HIPRAND_RNG_PSEUDO_DEFAULT );
-    hiprandSetPseudoRandomGeneratorSeed( gen, time( NULL ) );
-    hiprandGenerate( gen, (unsigned int *) d_x, n );
-    hiprandDestroyGenerator( gen );
-}
+template class AMP::GPUFunctionTable<double>;
+template class AMP::GPUFunctionTable<float>;
 
-template<>
-void GPUFunctionTable::rand<float>( size_t n, float *d_x )
-{
-    hiprandGenerator_t gen;
-    hiprandCreateGenerator( &gen, HIPRAND_RNG_PSEUDO_DEFAULT );
-    hiprandSetPseudoRandomGeneratorSeed( gen, time( NULL ) );
-    hiprandGenerateUniform( gen, d_x, n );
-    hiprandDestroyGenerator( gen );
-}
-
-template<>
-void GPUFunctionTable::rand<double>( size_t n, double *d_x )
-{
-    hiprandGenerator_t gen;
-    hiprandCreateGenerator( &gen, HIPRAND_RNG_PSEUDO_DEFAULT );
-    hiprandSetPseudoRandomGeneratorSeed( gen, time( NULL ) );
-    hiprandGenerateUniformDouble( gen, d_x, n );
-    hiprandDestroyGenerator( gen );
-}
-
-template double
-AMP::GPUFunctionTable::sum<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>>(
-    AMP::Array<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>> const & );
-template void AMP::GPUFunctionTable::
-    transformReLU<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>>(
-        AMP::Array<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>> const &,
-        AMP::Array<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>> & );
-template void AMP::GPUFunctionTable::
-    transformAbs<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>>(
-        AMP::Array<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>> const &,
-        AMP::Array<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>> & );
-template void AMP::GPUFunctionTable::
-    transformHardTanh<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>>(
-        AMP::Array<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>> const &,
-        AMP::Array<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>> & );
-template void AMP::GPUFunctionTable::
-    transformTanh<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>>(
-        AMP::Array<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>> const &,
-        AMP::Array<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>> & );
-template void AMP::GPUFunctionTable::
-    transformSigmoid<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>>(
-        AMP::Array<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>> const &,
-        AMP::Array<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>> & );
-template void AMP::GPUFunctionTable::
-    transformSoftPlus<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>>(
-        AMP::Array<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>> const &,
-        AMP::Array<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>> & );
-template bool
-AMP::GPUFunctionTable::equals<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>>(
-    AMP::Array<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>> const &,
-    AMP::Array<double, AMP::GPUFunctionTable, AMP::HipManagedAllocator<double>> const &,
-    double );
 } // namespace AMP
