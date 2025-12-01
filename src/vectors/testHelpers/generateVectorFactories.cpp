936e6b300eeba8d8283a182fc2261c93dc64501c
Bobby Philip <bphilip@lanl.gov>
2025-12-01 16:27:02 -0700

    undo change, separate MR

--- src/vectors/testHelpers/generateVectorFactories.cpp
+++ src/vectors/testHelpers/generateVectorFactories.cpp
@@ -341,7 +341,6 @@ std::vector<std::string> getSimpleVectorFactories()
     list.emplace_back( "SimpleVectorFactory<45,true,double>" );
     list.emplace_back( "SimpleVectorFactory<15,false,double,openmp,cpu>" );
     list.emplace_back( "SimpleVectorFactory<15,false,double,kokkos,cpu>" );
-    list.emplace_back( "SimpleVectorFactory<15,false,double,kokkos,gpu>" );
     // list.push_back( "SimpleVectorFactory<15,false,double,default,gpu>" ); // Requires UVM
     list.emplace_back( "SimpleVectorFactory<15,false,double,gpu,gpu>" );
     list.emplace_back( "SimpleVectorFactory<15,false,float>" );
