4536a6f3e3e39c38fb3203826d4fefd081fe70d5
Grace Lydia Longo - 379751 <glongo@darwin-fe3.lanl.gov>
2025-12-17 09:03:57 -0700

    remove stuff thats already listed in the builtin libmesh

--- spack-repo/v1/packages/libmesh/package.py
+++ spack-repo/v1/packages/libmesh/package.py
@@ -10,11 +10,5 @@ class Libmesh(Libmesh):
     this inherits from the builtin spack package for Libmesh in order to add version 1.8.4 as an option
     """
 
-    homepage = "https://libmesh.github.io/"
-    url = "https://github.com/libMesh/libmesh/releases/download/v1.0.0/libmesh-1.0.0.tar.bz2"
-    git = "https://github.com/libMesh/libmesh.git"
-
-    license("LGPL-2.1-or-later")
-
     version("1.8.4", commit="82c9bc42ae2296437bc751bb72830cfcf3fb405f", submodules=True)
  
