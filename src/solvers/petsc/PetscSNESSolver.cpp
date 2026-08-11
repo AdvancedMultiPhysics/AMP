1fe98661cc3e8354becfb542acba30a5e2607bcb
Ian May <imay@lanl.gov>
2026-08-11 08:48:03 -0600

    Don't write down fcn pointer wrong

--- src/solvers/petsc/PetscSNESSolver.cpp
+++ src/solvers/petsc/PetscSNESSolver.cpp
@@ -275,7 +275,7 @@ void PetscSNESSolver::initializePetscObjects()
     checkErr( SNESSetFromOptions( d_SNESSolver ) );
 
 #if PETSC_VERSION_LT( 3, 23, 0 )
-    typedef PetscCtxDestroyFn PetscErrorCode()( void * );
+    typedef PetscErrorCode ( *PetscCtxDestroyFn )( void * );
 #endif
 
     if ( d_bPrintNonlinearResiduals ) {
