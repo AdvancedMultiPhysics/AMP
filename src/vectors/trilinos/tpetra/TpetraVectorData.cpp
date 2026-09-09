dfbecc2f1362e80fb9db6a07673645bba8136166
Ian May <imay@lanl.gov>
2026-09-09 14:32:56 -0600

    Fix NodeType instantiations on Tpetra vector classes

--- src/vectors/trilinos/tpetra/TpetraVectorData.cpp
+++ src/vectors/trilinos/tpetra/TpetraVectorData.cpp
@@ -1,9 +1,11 @@
 #include "AMP/vectors/trilinos/tpetra/TpetraVectorData.hpp"
+#include "AMP/vectors/trilinos/tpetra/TpetraDefaults.hpp"
 
 namespace AMP::LinearAlgebra {
 
-template class TpetraVectorData<double, int32_t, int32_t>;
-template class TpetraVectorData<double, int32_t, int64_t>;
-template class TpetraVectorData<float, int32_t, int64_t>;
+template class TpetraVectorData<double, int32_t, int32_t, TpetraNT>;
+template class TpetraVectorData<double, int32_t, int64_t, TpetraNT>;
+template class TpetraVectorData<float, int32_t, int32_t, TpetraNT>;
+template class TpetraVectorData<float, int32_t, int64_t, TpetraNT>;
 
 } // namespace AMP::LinearAlgebra
--- src/vectors/trilinos/tpetra/TpetraVectorOperations.cpp
+++ src/vectors/trilinos/tpetra/TpetraVectorOperations.cpp
@@ -1,9 +1,11 @@
 #include "AMP/vectors/trilinos/tpetra/TpetraVectorOperations.hpp"
+#include "AMP/vectors/trilinos/tpetra/TpetraDefaults.hpp"
 
 namespace AMP::LinearAlgebra {
 
-template class TpetraVectorOperations<double, int32_t, int32_t>;
-template class TpetraVectorOperations<double, int32_t, int64_t>;
-template class TpetraVectorOperations<float, int32_t, int64_t>;
+template class TpetraVectorOperations<double, int32_t, int32_t, TpetraNT>;
+template class TpetraVectorOperations<double, int32_t, int64_t, TpetraNT>;
+template class TpetraVectorOperations<float, int32_t, int32_t, TpetraNT>;
+template class TpetraVectorOperations<float, int32_t, int64_t, TpetraNT>;
 
 } // namespace AMP::LinearAlgebra
